a585bb933ca7bd15250e8c2186df991ad4f03e8f
u2718 <lazarevm93@gmail.com>
2015-07-09 07:55:00 +0500

    В класс серии добавлено поле, содержащее дату выхода.

--- DAL/Series.cs
+++ DAL/Series.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DAL
 {
     public class Series
     {
         public String Title { get; set; }
-
+        public DateTime Date { get; set; }
     }
 }
