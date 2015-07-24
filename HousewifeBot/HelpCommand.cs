ba636f2928734a8645f405eaf1c3198ec2e69b51
nick6652 <nick6652@gmail.com>
2015-07-24 21:34:19 +0500

    Удалена отладочная команда
    Удалена отладочная команда /subscribe_all

--- HousewifeBot/HelpCommand.cs
+++ HousewifeBot/HelpCommand.cs
@@ -8,10 +8,9 @@ namespace HousewifeBot
         {
             try
             {
-                TelegramApi.SendMessage(Message.From, "Команды: \n" +
+                TelegramApi.SendMessage(Message.From, "Список команд: \n" +
                 "/serials - вывести список всех сериалов \n" +
                 "/subscribe - подписаться на сериал \n" +
-                "/subscribe_all - подписаться на ВСЕ сериалы!!! \n" +
                 "/unsubscribe - отписаться от сериала \n" +
                 "/unsubscribe_all - отписаться от всех сериалов \n" +
                 "/help - справка"
