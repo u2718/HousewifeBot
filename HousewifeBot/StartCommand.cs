fef0ded1ec93ae8da7dac1ec491d33a604d20aef
nick6652 <nick6652@gmail.com>
2015-07-24 17:24:12 +0500

    Добавлена команда '/start'

---
+++ HousewifeBot/StartCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HousewifeBot
+{
+    class StartCommand : Command
+    {
+        public override bool Execute()
+        {
+            try
+            {
+                TelegramApi.SendMessage(Message.From, $"Шалом, {Message.From.FirstName}. Введи '/help', чтобы посмотреть список команд.");
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
