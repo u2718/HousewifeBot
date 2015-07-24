d7c1be325dbe004f72e3720491dcc4ced45ec210
nick6652 <nick6652@gmail.com>
2015-07-24 17:23:55 +0500

    Добавлена команда '/help'

---
+++ HousewifeBot/HelpCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace HousewifeBot
+{
+    class HelpCommand : Command
+    {
+        public override bool Execute()
+        {
+            try
+            {
+                TelegramApi.SendMessage(Message.From, "Команды: \n" +
+                "/serials - вывести список всех сериалов \n" +
+                "/subscribe - подписаться на сериал \n" +
+                "/subscribe_all - подписаться на ВСЕ сериалы!!! \n" +
+                "/unsubscribe - отписаться от сериала \n" +
+                "/unsubscribe_all - отписаться от всех сериалов \n" +
+                "/help - справка"
+                );
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
