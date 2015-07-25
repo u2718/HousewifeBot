34f162b79f4784cd9a613862cba3a6e42b87bc8f
Lazarev Mikhail <lazarevm93@gmail.com>
2015-07-25 21:35:17 +0500

    Логирование команды '/subscribe_all'

--- HousewifeBot/SubscribeAllCommand.cs
+++ HousewifeBot/SubscribeAllCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DAL;
 
@@ -10,8 +11,18 @@ namespace HousewifeBot
         {
             using (AppDbContext db = new AppDbContext())
             {
-                User user = db.Users.FirstOrDefault(u => u.TelegramUserId == Message.From.Id);
-                bool newUser = false;
+                Program.Logger.Debug($"{GetType().Name}: Searching user with TelegramId: {Message.From.Id} in data base");
+
+                User user;
+                try
+                {
+                    user = db.Users.FirstOrDefault(u => u.TelegramUserId == Message.From.Id);
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"{GetType().Name}: An error occurred while searching user in data base", e);
+                }
+
                 if (user == null)
                 {
                     user = new User
@@ -21,10 +32,48 @@ namespace HousewifeBot
                         LastName = Message.From.LastName,
                         Username = Message.From.Username
                     };
-                    newUser = true;
+
+                    Program.Logger.Info($"{GetType().Name}: {user.FirstName} {user.LastName} is new User");
+
+                    Program.Logger.Debug($"{GetType().Name}: Adding user {user.FirstName} {user.LastName} to data base");
+                    try
+                    {
+                        db.Users.Add(user);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception($"{GetType().Name}: An error occurred while adding user to data base", e);
+                    }
+                }
+                else
+                {
+                    Program.Logger.Debug($"{GetType().Name}: User {user.FirstName} {user.LastName} is already exist");
                 }
 
-                foreach (Show show in db.Shows)
+                List<Show> shows;
+                Program.Logger.Debug($"{GetType().Name}: Retrieving serials list");
+                try
+                {
+                    shows = db.Shows.ToList();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"{GetType().Name}: An error occurred while retrieving serials list", e);
+                }
+
+                List<Subscription> subscriptions;
+                Program.Logger.Debug($"{GetType().Name}: Retrieving subscriptions of {user.FirstName} {user.LastName}");
+                try
+                {
+                    subscriptions = db.Subscriptions.Where(s => s.User.Id == user.Id).ToList();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"{GetType().Name}: An error occurred while retrieving subscriptions", e);
+                }
+
+                Program.Logger.Debug($"{GetType().Name}: Subscribing {user.FirstName} {user.LastName} to all serials");
+                foreach (Show show in shows)
                 {
                     Subscription subscription = new Subscription
                     {
@@ -33,21 +82,40 @@ namespace HousewifeBot
                         SubscriptionDate = DateTime.Now
                     };
 
-                    if (newUser)
+                    if (!subscriptions.Any(s => Equals(s.Show, show)))
                     {
-                        user.Subscriptions.Add(subscription);
-                        db.Users.Add(user);
-                        newUser = false;
-                    }
-                    else if (!db.Subscriptions.Any(s => s.User.Id == user.Id && s.Show.Id == show.Id))
-                    {
-                        db.Subscriptions.Add(subscription);
+                        try
+                        {
+                            db.Subscriptions.Add(subscription);
+                        }
+                        catch (Exception e)
+                        {
+                            throw new Exception($"{GetType().Name}: An error occurred while subscribing {user.FirstName} {user.LastName} to {show.OriginalTitle}", e);
+                        }
                     }
                 }
 
-                db.SaveChanges();
+                Program.Logger.Debug($"{GetType().Name}: Saving changes to data base");
+                try
+                {
+                    db.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    throw new Exception($"{GetType().Name}: An error occurred while saving changes to data base", e);
+                }
             }
-            TelegramApi.SendMessage(Message.From, "Вы, братишка, подписаны на все сериалы");
+            Program.Logger.Debug($"SubscribeCommand: Sending response to {Message.From.FirstName} {Message.From.LastName}");
+            try
+            {
+                TelegramApi.SendMessage(Message.From, "Вы, братишка, подписаны на все сериалы");
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"SubscribeCommand: An error occurred while sending response to {Message.From.FirstName} {Message.From.LastName}", e);
+            }
+
+            Status = true;
             return true;
         }
     }
