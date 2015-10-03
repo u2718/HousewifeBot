25ec45d9ad03e5342ebd877e4c2f5e56ef8802e0
Lazarev Mikhail <lazarevm93@gmail.com>
2015-10-03 20:38:12 +0500

    Логирование загрузки торрентов

--- TorrentDownloader/UTorrentDownloader .cs
+++ TorrentDownloader/UTorrentDownloader .cs
@@ -2,12 +2,15 @@
 using System.Net;
 using System.Net.Http;
 using System.Text.RegularExpressions;
+using NLog;
 
 namespace TorrentDownloader
 {
     public class UTorrentDownloader : ITorrentDownloader
     {
         private const string ErrorResultString = "\r\ninvalid request";
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+
         public bool Download(Uri torrent, Uri torrenWebUiUri, string password)
         {
             CookieContainer cc = new CookieContainer();
@@ -24,14 +27,28 @@ namespace TorrentDownloader
                 string tokenHtml;
                 try
                 {
+                    logger.Debug($"{GetType().Name}: Retrieving token page");
                     tokenHtml = client.GetAsync(torrenWebUiUri + $"token.html?t={CurrentUnixTime()}").Result.Content.ReadAsStringAsync().Result;
                 }
                 catch (Exception e)
                 {
+                    logger.Error(e, $"{GetType().Name}: An error occurred while retrieving token page");
                     return false;
                 }
-                string token = Regex.Match(tokenHtml, @"'>(.+?)<\/").Groups[1].Value;
 
+                logger.Debug($"{GetType().Name}: Parsing token page");
+                string token;
+                try
+                {
+                    token = Regex.Match(tokenHtml, @"'>(.+?)<\/").Groups[1].Value;
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e, $"{GetType().Name}: An error occurred while parsing token page");
+                    return false;
+                }
+
+                logger.Debug($"{GetType().Name}: Adding torrent using WebUi");
                 try
                 {
                     result = client.GetAsync(torrenWebUiUri + $"?token={token}&action=add-url&s={Uri.EscapeDataString(torrent.ToString())}&t={CurrentUnixTime()}")
@@ -39,10 +56,12 @@ namespace TorrentDownloader
                 }
                 catch (Exception e)
                 {
+                    logger.Error(e, $"{GetType().Name}: An error occurred while adding torrent");
                     return false;
                 }
             }
 
+            logger.Debug($"{GetType().Name}: result: {result}");
             return result != ErrorResultString;
         }
 
