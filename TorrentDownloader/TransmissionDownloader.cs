caa5e1274a1a5f05047718e5ca9e69199ce2efe6
Lazarev Mikhail <lazarevm93@gmail.com>
2016-07-09 14:53:34 +0500

    Загрузка через Transmission

---
+++ TorrentDownloader/TransmissionDownloader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace TorrentDownloader
+{
+    public class TransmissionDownloader : ITorrentDownloader
+    {
+        public bool Download(Uri torrent, Uri torrenWebUiUri, string password)
+        {
+            var handler = new HttpClientHandler()
+            {
+                Credentials = new NetworkCredential("admin", password),
+                CookieContainer = new CookieContainer(),
+                UseCookies = true
+            };
+
+            using (HttpClient client = new HttpClient(handler))
+            {
+                var requestMessage = new HttpRequestMessage(HttpMethod.Post, new Uri(torrenWebUiUri, "rpc"));
+                var request = new
+                {
+                    method = "torrent-add",
+                    arguments = new
+                    {
+                        paused = false,
+                        filename = torrent.AbsoluteUri
+                    }
+                };
+
+                requestMessage.Content = new StringContent(JsonConvert.SerializeObject(request));
+                dynamic response = JsonConvert.DeserializeObject(client.SendAsync(requestMessage).Result.Content.ReadAsStringAsync().Result);
+                return string.Equals(response.result.ToString(), "success", StringComparison.OrdinalIgnoreCase);
+            }
+        }
+    }
+}
